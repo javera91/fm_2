58b8645d1dea951b2e6e5ee15f795b3fc11208bf
javera91 <146044960+javera91@users.noreply.github.com>
2023-12-19 20:45:16 -0500

    c2
    c2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'fm_2do' title`, () => {
+  it(`should have the 'fm_2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fm_2do');
+    expect(app.title).toEqual('fm_2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fm_2do');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fm_2');
   });
 });
